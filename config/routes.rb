ce42df0b17b7ec211f2fd0168fd88af3e99c9862
Sam Peterson <speterson96@student.cvtc.edu>
2018-02-17 18:05:59 +0000

    Added website structure and some css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/about', to: 'home#about', as: 'about'
+  root to: 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
