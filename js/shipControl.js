86eb70f79d088092ea3ec12d6ee596689ddf9e70
Derik Beardshear <derikbeardshear@Deriks-MacBook-Pro.local>
2018-02-20 14:29:08 -0600

    Ship now shows up, but movement is slightly functioning.

--- js/shipControl.js
+++ js/shipControl.js
@@ -13,11 +13,12 @@ var states = {
 
 var graphicAssets = {
     
-    ship:{URL:'../assets/theHoff.jpg', name:'ship'},
+    ship:{URL:'/assets/theHoff.jpg', name:'ship'},
     
 };
 
 var shipProperties = {
+    
     startX: gameProperties.screenWidth * 0.5,
     startY: gameProperties.screenHeight * 0.5,
     acceleration: 300,
@@ -28,11 +29,12 @@ var shipProperties = {
 };
 
 var gameState = function (game){
+    
     this.shipSprite;
     
-    this.mouse_left;
-    this.mouse_right;
-    this.mouse_thrust;
+    this.key_left;
+    this.key_right;
+    this.key_thrust;
     
 };
 
@@ -47,7 +49,7 @@ gameState.prototype = {
         
         this.initGraphics();
         this.initPhysics();
-        this.initMouse();
+        this.initKeyboard();
         
     },
 
@@ -56,47 +58,59 @@ gameState.prototype = {
     },
     
     initGraphics: function () {
+        
         this.shipSprite = game.add.sprite(shipProperties.startX, shipProperties.startY, graphicAssets.ship.name);
         this.shipSprite.angle = -90;
         this.shipSprite.anchor.set(0.5, 0.5);
+    
     },
     
     initPhysics: function() {
         
-        game.physics.startSystem(Phaser.Physics.Arcade);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        game.physics.enable(this.shipSprite, Phaser.Physics.Arcade);
+        game.physics.enable(this.shipSprite, Phaser.Physics.ARCADE);
         this.shipSprite.body.drag.set(shipProperties.drag);
         this.shipSprite.body.maxVelocity.set(shipProperties.maxVelocity);
         
     },
     
-    initMouse: function() {
+    initKeyboard: function() {
         
-        this.mouse_left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        this.mouse_right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        this.mouse_thrust = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        this.key_left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.key_right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        this.key_thrust = game.input.keyboard.addKey(Phaser.Keyboard.UP);
         
     },
     
     checkPlayerInput: function () {
         
        if (this.key_left.isDown) {
-            this.shipSprite.body.angularVelocity = -shipProperties.angularVelocity;
-        } else if (this.key_right.isDown) {
+            
+           this.shipSprite.body.angularVelocity = -shipProperties.angularVelocity;
+        
+       } else if (this.key_right.isDown) {
+           
             this.shipSprite.body.angularVelocity = shipProperties.angularVelocity;
-        } else {
-            this.shipSprite.body.angularVelocity = 0;
-        }
+        
+       } else {
+       
+           this.shipSprite.body.angularVelocity = 0;
+        
+       }
         
         if (this.key_thrust.isDown) {
-            game.physics.arcade.accelerationFromRotation(this.shipSprite.rotation, shipProperties.acceleration, this.shipSprite.body.acceleration);
-        } else {
-            this.shipSprite.body.acceleration.set(0);
-        }
-    },  
+       
+            game.physics.arcade.accelerationFromRotation(this.shipSprite.rotation, 
+            shipProperties.acceleration, this.shipSprite.body.acceleration);
         
-    },
+        } else {
+        
+            this.shipSprite.body.acceleration.set(0);
+        
+        }
+        
+    },  
     
 };
 
