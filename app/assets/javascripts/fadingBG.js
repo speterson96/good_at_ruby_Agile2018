5f127a2233f054085ce95e8947d16cf27c299009
Alex Tanner <atanner6@student.cvtc.edu>
2018-02-21 02:27:00 +0000

    Fixed some styles and added fading background image

---
+++ app/assets/javascripts/fadingBG.js
@@ -0,0 +1,6 @@
+  $('.fadein img:gt(0)').hide();
+    setInterval(function(){   
+      $('.fadein :first-child').fadeOut(2000)
+         .next('img').fadeIn(2000)
+         .end().appendTo('.fadein');}, 
+    5000); 	
