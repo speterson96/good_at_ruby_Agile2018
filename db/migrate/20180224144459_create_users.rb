d82e7cf84c0326085350fb34faa5bca3d704e25f
Benjamin Miles <dootbin@gmail.com>
2018-02-24 09:48:51 -0600

    house keeping

---
+++ db/migrate/20180224144459_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :image
+      t.string :token
+      t.string :secret
+      t.string :profile_image
+
+      t.timestamps null: false
+    end
+  end
+end
