cd8d7251f1d9a2e1ea6996580027778869c4b296
Alex Tanner <atanner6@student.cvtc.edu>
2018-02-26 21:37:17 +0000

    Implemented logic for ajax

--- app/assets/javascripts/saveUserState.js
+++ app/assets/javascripts/saveUserState.js
@@ -1,24 +1,43 @@
 
 var saveUserState =  {
-	
-	userLives : 0,
-	userBullets : 0,
-	userScore : 0,
-	userDifficulty : 'hard',
-		
-	init : function(lives, bullets, score, difficulty){
-		this.userLives : lives;
-		this.userBullets : bullets;
-		this.userScore : score;
-	}
-	
-	 sendUserScore : function(lives, bullets, score) {
+
+	 sendUserSaveState : function(lives, bullets, score, difficulty) {
 		 
-		 // Work magic here.
+		$.ajax({
+      url: "/",
+      type: "get",
+      data: {
+        user_lives : lives, 
+        user_bullets : bullets, 
+        user_score : score, 
+        user_difficulty : difficulty
+      },
+      success: function(){
+        alert('Saved Successfully :)');
+      },
+       error: function(xhr,status,error){
+         console.log(xhr);
+         //alert(error); Dont know what the problem is yet
+      }
+    });
 		 
-	 }
+	 },
 	
-	// Example
-	// saveUserState.int(3, 5, 2500, "hard");
-	// saveUserState.sendUserScore();
+	sendUserScore : function(score) {
+		 
+		$.ajax({
+      url: "/sendScores",
+      type: "get",
+      data: {user_score: score},
+      success: function(){
+        alert('Saved Successfully :)');
+      },
+       error: function(xhr,status,error){
+         console.log(xhr);
+         //alert(error); Dont know what the problem is yet
+      }
+    });
+		 
+  }
+
 } 
