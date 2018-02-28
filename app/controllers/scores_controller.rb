1d254ac639a2e118543bd3109481e54b54379654
Alex Tanner <atanner6@student.cvtc.edu>
2018-02-28 01:17:55 +0000

    Adjusted what data is passed into the score.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -13,7 +13,7 @@ class ScoresController < ApplicationController
   
   def create
     userScore = params[:user_score]   
-    @scores = Score.create(user_id: 1, score: userScore)
+    @scores = Score.create(name: "Mr. Name", score: userScore)
     if @scores.save
       respond_to do |format|
          format.html {redirect_to root_path}
