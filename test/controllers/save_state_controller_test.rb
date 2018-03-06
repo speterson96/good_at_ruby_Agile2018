740fc03e3e183c85948735909833a8cce3a43ecb
Alex Tanner <atanner6@student.cvtc.edu>
2018-03-06 19:30:19 +0000

    Fixed fixtures

--- test/controllers/save_state_controller_test.rb
+++ test/controllers/save_state_controller_test.rb
@@ -3,8 +3,8 @@ require './test/test_helper'
 class ScoresControllerTest < ActionController::TestCase
 
   test "should send save" do
-  controller = scores_controller.new 
-    assert_that  controller.create
+    assert_recognizes({:controller => 'save_state', :action => 'create'}, {:path => 'sendSaveState', :method => :get})
+     #get "/sendSaveState", controller: 'save_state', action: 'create' -- Route path its looking for
   end
   
 end
