44a4f04e0ee23013fdfb67af5cc1fe9b0a59d778
Alex Tanner <atanner6@student.cvtc.edu>
2018-03-14 02:46:14 +0000

    Set styles for static background image and added a bit of content to index and about aswell as a few other style adjusments

--- app/assets/javascripts/fadingBG.js
+++ app/assets/javascripts/fadingBG.js
@@ -1,9 +1,10 @@
- 
  /*
+ $(function(){
  $('.fadein img:gt(0)').hide();
     setInterval(function(){   
       $('.fadein :first-child').fadeOut(2000)
          .next('img').fadeIn(2000)
          .end().appendTo('.fadein');}, 
     5000);
-    */
+});
+*/
